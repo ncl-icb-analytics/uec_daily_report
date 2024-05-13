19ea197ce343101be22909ecf7f55cf1e8c1b661
Jake Kealey <jake.kealey@nhs.net>
2024-05-13 11:26:39 +0100

    Pushed inter saving out to main ingestion for collating

--- src/main_ingestion.py
+++ src/main_ingestion.py
@@ -15,7 +15,10 @@ load_dotenv(override=True)
 
 ### Generate file for intermediate wrangle:
 
-pd.DataFrame([], columns=['source', 'indicatorKeyName', 'siteId', 'ReportDate', 'metric_type', 'value']).to_csv('inter.csv', mode='w', index=False, header=True)
+pd.DataFrame([], 
+             columns=['source', 'indicatorKeyName', 'site_code_ref', 
+                      'ReportDate', 'metric_type', 'value']
+             ).to_csv('inter.csv', mode='w', index=False, header=True)
 
 '''
 Pull from smart API
@@ -26,7 +29,8 @@ print("API program starting...")
 ### Import settings from the .env file
 env = import_settings(config)
 
-### Process the settings to get the start and end dates & determine how many runs are needed to get all the data
+### Process the settings to get the start and end dates & 
+### determine how many runs are needed to get all the data
 date_end = process_date_end(env["DATE_END"])
 date_start = process_date_window(env["DATE_WINDOW"], date_end)
 runs = calculate_runs(date_start, date_end)
@@ -68,16 +72,17 @@ for run in runs:
                 res = smart_request(url, key, date_start, date_end, site)
             except:
                 raise Exception("Failed twice so cancelling execution.")
-        print(f"Request fulfilled for site {site} from {date_start} to {date_end}")
+        print(f"Request fulfilled for site {site}",
+              " from {date_start} to {date_end}")
 
         ##Convert API response data into universal format for future work
-        processing_data_for_storage(config, res, date_start, date_end)
-        
-        #Upload and manage datasets - Broken??
-        query_del = get_delete_query(date_start, date_end, site, env)
-        upload_request_data(res, query_del, date_start, date_end, site, env)  # this needs to be generic enough for all 4 pipelines
+        inter = processing_data_for_storage(config, res, date_start, date_end)
 
-        #DEBUG LINE
-        break
+        inter.to_csv('inter.csv', mode='a', index=False, header=False)
+
+        #Upload and manage datasets - Very fixed!!
+        query_del = get_delete_query(date_start, date_end, site, env)
+        # this needs to be generic enough for all 4 pipelines
+        upload_request_data(res, query_del, date_start, date_end, site, env)  
 
 print("All API pulls complete")
