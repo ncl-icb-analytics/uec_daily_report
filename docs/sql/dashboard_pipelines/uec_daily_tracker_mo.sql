0c5fff00a0f3a6d50f595ea0b56ed29e2e5b50b1
Jake Kealey <jake.kealey@nhs.net>
2024-05-16 20:15:54 +0100

    Joined to get frontend provider name and filtered out CFH

--- docs/sql/dashboard_pipelines/uec_daily_tracker_mo.sql
+++ docs/sql/dashboard_pipelines/uec_daily_tracker_mo.sql
@@ -1,8 +1,14 @@
 SELECT 
 	date_data,
 	MONTH(date_data) AS month_data,
-	provider,
+	org.g_acute_sites AS shorthand,
 	metric_name,
 	metric_value
-FROM [Data_Lab_NCL_Dev].[JakeK].[uec_daily_tracker_mo]
+FROM [Data_Lab_NCL_Dev].[JakeK].[uec_daily_tracker_mo] mo
+
+LEFT JOIN [Data_Lab_NCL_Dev].[JakeK].[orgs] org
+ON org.site_code = mo.provider_code
+AND g_acute_sites IS NOT NULL
+
 WHERE date_data >= DATEADD(d, -40, GETDATE())
+AND g_acute_sites != 'CFH'
