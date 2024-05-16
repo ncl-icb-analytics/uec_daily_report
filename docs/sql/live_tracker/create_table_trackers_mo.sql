d369a57a1895693b75aa2c25f6912fe4c3ec22d0
Jake Kealey <jake.kealey@nhs.net>
2024-05-16 20:16:27 +0100

    Modified to use provider code instead of provider name to allow for joining on the org reference table to get the universial front end names.

--- docs/sql/live_tracker/create_table_trackers_mo.sql
+++ docs/sql/live_tracker/create_table_trackers_mo.sql
@@ -2,9 +2,9 @@
 CREATE TABLE [Data_Lab_NCL_Dev].[JakeK].[uec_daily_tracker_mo] (
 	date_extract DATE NOT NULL,
 	date_data DATE NOT NULL,
-	provider CHAR(20) NOT NULL,
+	provider_code CHAR(5) NOT NULL,
 	metric_name CHAR(20) NOT NULL,
 	metric_value INT NOT NULL
 
-	PRIMARY KEY (date_data, provider, metric_name)
+	PRIMARY KEY (date_data, provider_code, metric_name)
 )
